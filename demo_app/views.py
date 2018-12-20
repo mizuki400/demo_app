2c1e21a4e93d049f930bcb96c321453812cdf0f2
才川瑞樹 <saikawamizuki@MacBooklocal.local>
2018-12-21 02:49:05 +0900

    forth commit

--- demo_app/views.py
+++ demo_app/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .forms import InputForm
+from .models import Customers
 
 # Create your views here.
 def index(request):
-    return render(request, 'demo_app/index.html', {})
+#    return render(request, 'demo_app/index.html', {})
+    if request.method == 'POST':
+        if "to_input" in request.POST:
+            input_form()
+    else:
+         return render(request, 'demo_app/index.html', {})
+
+def input_form(request):
+    if request.method == 'POST':
+        form = InputForm(request.POST)
+        if form.is_valid():
+            form.save() # 入力された値を保存
+            return redirect('result')
+    else:
+        form = InputForm()
+        return render(request,'demo_app/input_form.html',{'form':form})
+
+
+def result(request):
+    return render(request , 'demo_app/result.html', {})
